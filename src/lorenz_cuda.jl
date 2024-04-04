711599ef40f80d5f4e27cbc270f4e6a93c5deb0b
samminweng <samminweng@gmail.com>
2024-04-04 17:42:52 +1300

    Updated GPU kernel without using index

--- src/lorenz_cuda.jl
+++ src/lorenz_cuda.jl
@@ -4,18 +4,17 @@ Pkg.add("BenchmarkTools") # Install benchmark tools
 Pkg.add("BenchmarkPlots")
 Pkg.add("StatsPlots")
 Pkg.add("Plots") # Install `Plots` package
-Pkg.add("Images") 
-Pkg.add("FileIO")
+
 
 using Plots, Images, FileIO
 # Use Cuda
 using CUDA, Test, BenchmarkTools, BenchmarkPlots, StatsPlots
 # println("CUDA information\n", CUDA.versioninfo())
 # Reference: https://pde-on-gpu.vaw.ethz.ch/lecture1/#solving_partial_differential_equations_in_parallel_on_gpus
-CUDA.allowscalar(true)
+# CUDA.allowscalar(true)
 
 const DIM = 3 # Specify the dimensions
-const STEPS = 10 
+const STEPS = 100 
 # # integrate dx/dt = lorenz(t,x) numerically for 10 steps
 function lorenz(x)
     σ = 10
@@ -31,7 +30,7 @@ function plot_loren(out, device)
     plot(out[1,:], out[2,:], out[3,:])
     path = "images/lorenz_$device.png"
     savefig(path)
-    print("Save $device result to $path")
+    println("Save $device result to $path")
     # initialize a 3D plot with 1 empty series
     plt = plot3d(
         1,
@@ -49,41 +48,71 @@ function plot_loren(out, device)
     end every 1
     path = "images/lorenz_$device.gif"
     gif(anim, path, fps=1)
-    print("Save $device result to $path")
+    println("Save $device result to $path")
+end
+
+function lorenz_gpu(x, y, z)
+    σ = 10
+    β = 8/3
+    ρ = 28
+    return CuArray([σ*(y-x), x*(ρ-z) - y, x*y - β*z])
 end
 
 # Animate lorenz system from step 2 upto the final steps
-function gpu_step!(x)
-    dt = 0.01
-    lz = lorenz(x)
-    for i in eachindex(x)
-        @inbounds x[i] = x[i] + lz[i] * dt
-    end
-    # @cuprintln("x $x")
+function gpu_loren!(x, lz, dt)
+    i = threadIdx().x
+    x[i] = x[i] + lz[i] * dt[i]
     return nothing
 end
 
+function gpu_step!(out_d)
+    dt = CUDA.fill(0.01f0, 3)
+    # Simulate each step 
+    for step = 2:STEPS
+        x = out_d[:, step-1]
+        lz = lorenz_gpu(x[1], x[2], x[3])
+        @cuda threads=length(x) gpu_loren!(x, lz, dt)
+        out_d[:, step] = x
+        lz=nothing
+    end
+    dt = nothing; 
+    return nothing
+end
 
 function gpu_benchmark()
-    println("Start running LORENZ on GPUs")
-    # Create 'x' and out on GPU device
     x_d = CUDA.fill(0.0f0, 3)
     out_d = CUDA.fill(0.0f0, (3, STEPS))
     x_d[1]= 2.0
     println(" size(out_d) = ", size(out_d))
     # Initial 'out' values
+    out_d[:, 1] = x_d    
+    b = @benchmark gpu_step!(out_d) setup=(out_d=copy($out_d)) evals=1 samples=1
+    io = IOContext(stdout, :histmin=>1, :histmax=>2, :logbins=>true)
+    # using BenchmarkPlots, StatsPlots
+    println("Benchmark results for CUDA 'gpu_step!' ")
+    println("Minimal time of CUDA gpu_step! = ", minimum(b))
+    println("Mean time of CUDA gpu_step! = ", mean(b))
+    println("Maximum time of CUDA gpu_step! = ", maximum(b))
+    show(io, MIME("text/plain"), b) 
+end
+
+function gpu_run()
+    println("Start running LORENZ on GPUs")
+    # Create 'x' and out on GPU device
+    x_d = CuArray([2.0, 0.0, 0.0])
+    out_d = CUDA.fill(0.0f0, (3, STEPS))
+    println(" size(out_d) = ", size(out_d))
+    # Initial 'out' values
     out_d[:, 1] = x_d
-    println("out_d = ", out_d)    
-    for step = 2:STEPS
-        x = copy(out_d[:, step-1])
-        gpu_step!(x)
-        out_d[:, step] = x
-        x = nothing
-    end
+    gpu_step!(out_d)
+    GC.gc(true) # Clear memory
+    println(CUDA.memory_status())
     # Run the lorenz on GPUs
     out = Array(out_d)
-    println("out = ", out, " Vector size 'out' = ", size(out))
+    println(" Vector size 'out' = ", size(out))
     plot_loren(out, "gpu")
+    out = nothing; GC.gc(true) # Clear memory
+    println(CUDA.memory_status())
 end
 
 
@@ -95,19 +124,37 @@ function cpu_step!(out)
     end
     return nothing
 end
-
-function cpu_benchmark()
+ # Run Benchmark on CPUs
+function cpu_benchmarks()   
     x = zeros(DIM)
     x[1] = 2.0
-    out = zeros(3, STEPS) # 3x500 arrays
-    println(" size(out) = ", size(out))
+    out = zeros(3, STEPS)
+    out[:, 1] = x 
+    io = IOContext(stdout, :histmin=>0.001, :histmax=>300, :logbins=>true)
+    b = @benchmark cpu_step!(out) setup=(out=copy($out)) evals=1 samples=1
+    println("Benchmark results for 'cpu_step!' ")
+    println("Minimal time of cpu_step! = ", minimum(b))
+    println("Mean time of cpu_step! = ", mean(b))
+    println("Maximum time of cpu_step! = ", maximum(b))
+    # plot(b)
+    show(io, MIME("text/plain"), b)
+end
+
+function cpu_run()
+    x = zeros(DIM)
+    x[1] = 2.0
+    out = zeros(3, STEPS) # 3xSTEP arrays
     out[:, 1] = x 
     cpu_step!(out)
     # println("out = ", out)
     plot_loren(out, "cpu")
+    out = nothing; GC.gc(true) # Clear memory
+    println(CUDA.memory_status())
 end
 
 # Perform LORENZ on CPUs
-# cpu_benchmark()
+# cpu_run()
+# cpu_benchmarks()
 # Perform LORENZ on GPUS
-gpu_benchmark()
+gpu_run()
+# gpu_benchmark()
